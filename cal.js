d2e356eaace6d9b5249c32d2df42dbed93f80eec
pandugalasrividya <pandugalasrividya@gmail.com>
2026-05-04 14:30:10 +0530

    basic calculator

---
+++ cal.js
@@ -0,0 +1,20 @@
+let a=45
+let b=56
+let operator="+";
+
+switch(operator){
+    case "+":
+        console.log(a+b)
+        break;
+    case "-":
+        console.log(a-b)
+        break;
+    case "*":
+        console.log(a*b)
+        break;
+    case "/":
+        console.log(a/b)
+        break;
+    default:
+        console.log("invalid operator")
+}
